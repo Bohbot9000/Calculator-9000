00c0feeacaa4c4c11decb0fb6907b7640de1f069
Bohbot9000 <47096264+Bohbot9000@users.noreply.github.com>
2019-01-27 22:00:37 -0600

    Add files via upload

---
+++ Calculator 9000/Calculator 9000/frmCalculator.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Calculator_9000
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculator));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMaximum = new System.Windows.Forms.Button();
+            this.btnMinimum = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtOp2 = new System.Windows.Forms.TextBox();
+            this.txtOp1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("OpenDyslexicMono", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblTitle.Location = new System.Drawing.Point(26, 27);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(192, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Calculator 9000";
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(88, 47);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(35, 23);
+            this.btnSubtract.TabIndex = 1;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAdd.Location = new System.Drawing.Point(21, 47);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(35, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(88, 79);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(35, 23);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMaximum
+            // 
+            this.btnMaximum.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnMaximum.Location = new System.Drawing.Point(88, 133);
+            this.btnMaximum.Name = "btnMaximum";
+            this.btnMaximum.Size = new System.Drawing.Size(35, 23);
+            this.btnMaximum.TabIndex = 5;
+            this.btnMaximum.Text = "Max";
+            this.btnMaximum.UseVisualStyleBackColor = true;
+            // 
+            // btnMinimum
+            // 
+            this.btnMinimum.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnMinimum.Location = new System.Drawing.Point(21, 133);
+            this.btnMinimum.Name = "btnMinimum";
+            this.btnMinimum.Size = new System.Drawing.Size(35, 23);
+            this.btnMinimum.TabIndex = 4;
+            this.btnMinimum.Text = "Min";
+            this.btnMinimum.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnMultiply.Location = new System.Drawing.Point(21, 76);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(35, 23);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Location = new System.Drawing.Point(21, 162);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(35, 23);
+            this.btnSquare.TabIndex = 6;
+            this.btnSquare.Text = "Sqrt";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(368, 238);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(64, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(278, 238);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(64, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSquare);
+            this.groupBox1.Controls.Add(this.btnSubtract);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.btnMultiply);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnDivide);
+            this.groupBox1.Controls.Add(this.btnMinimum);
+            this.groupBox1.Controls.Add(this.btnMaximum);
+            this.groupBox1.Location = new System.Drawing.Point(266, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 191);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Keys";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Functions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Operations";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Enter Operand 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Enter Operand 1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Brendan Bohlin";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(126, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 3);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "-";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(140, 165);
+            this.txtResult.MaxLength = 15;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(78, 20);
+            this.txtResult.TabIndex = 18;
+            // 
+            // txtOp2
+            // 
+            this.txtOp2.Location = new System.Drawing.Point(140, 123);
+            this.txtOp2.MaxLength = 5;
+            this.txtOp2.Name = "txtOp2";
+            this.txtOp2.Size = new System.Drawing.Size(78, 20);
+            this.txtOp2.TabIndex = 1;
+            // 
+            // txtOp1
+            // 
+            this.txtOp1.Location = new System.Drawing.Point(140, 91);
+            this.txtOp1.MaxLength = 5;
+            this.txtOp1.Name = "txtOp1";
+            this.txtOp1.Size = new System.Drawing.Size(78, 20);
+            this.txtOp1.TabIndex = 0;
+            // 
+            // frmCalculator
+            // 
+            this.AcceptButton = this.btnClear;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnAdd;
+            this.ClientSize = new System.Drawing.Size(487, 285);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtOp1);
+            this.Controls.Add(this.txtOp2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCalculator";
+            this.Text = "Calculator 9000";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMaximum;
+        private System.Windows.Forms.Button btnMinimum;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtOp2;
+        private System.Windows.Forms.TextBox txtOp1;
+    }
+}
+
---
+++ Calculator 9000/Calculator 9000/frmCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_9000
+{
+    public partial class frmCalculator : Form
+    {
+        //Project: Calculator - C01
+        //Programmer: Brendan Bohlin
+        //Date: 1/27/2019
+        //Description: Simple calculator program
+
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string txtOp1String = txtOp1.Text;
+            string txtOp2String = txtOp2.Text;
+
+            decimal op1 = decimal.Parse(txtOp1String);
+            decimal op2 = decimal.Parse(txtOp2String);
+
+            decimal result = Math.Round(op1 + op2, 3, MidpointRounding.AwayFromZero);
+            string resultString = result.ToString();
+            txtResult.Text = resultString;
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            string txtOp1String = txtOp1.Text;
+            string txtOp2String = txtOp2.Text;
+
+            decimal op1 = decimal.Parse(txtOp1String);
+            decimal op2 = decimal.Parse(txtOp2String);
+
+            decimal result = Math.Round(op1 - op2, 3, MidpointRounding.AwayFromZero);
+            string resultString = result.ToString();
+            txtResult.Text = resultString;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            string txtOp1String = txtOp1.Text;
+            string txtOp2String = txtOp2.Text;
+
+            decimal op1 = decimal.Parse(txtOp1String);
+            decimal op2 = decimal.Parse(txtOp2String);
+       
+            decimal result = Math.Round(op1 * op2, 3, MidpointRounding.AwayFromZero);
+            string resultString = result.ToString();
+            txtResult.Text = resultString;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            string txtOp1String = txtOp1.Text;
+            string txtOp2String = txtOp2.Text;
+
+            decimal op1 = decimal.Parse(txtOp1String);
+            decimal op2 = decimal.Parse(txtOp2String);
+
+            decimal result = Math.Round(op1 / op2 , 3, MidpointRounding.AwayFromZero);
+            string resultString = result.ToString();
+            txtResult.Text = resultString;
+
+        }
+    }
+}
